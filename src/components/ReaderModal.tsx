a4aa4465cc0f044d6fb2d181edd53867570b9c0f
surslofficial-pixel <surslofficial@gmail.com>
2026-03-10 09:54:22 +0500

    fix: auto format groups on mobile keyboards with inputMode

--- src/components/ReaderModal.tsx
+++ src/components/ReaderModal.tsx
@@ -105,11 +105,15 @@ export function ReaderModal({ bookId, onClose }: ReaderModalProps) {
                                     <BookOpen className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-slate-400" />
                                     <input
                                         type="text"
+                                        inputMode="numeric"
+                                        pattern="[0-9\-]*"
+                                        maxLength={5}
                                         value={selectedGuruh}
                                         onChange={(e) => {
                                             let val = e.target.value.replace(/\D/g, '');
+                                            if (val.length > 4) val = val.slice(0, 4);
                                             if (val.length > 1) {
-                                                val = val.slice(0, 1) + '-' + val.slice(1, 4);
+                                                val = val.slice(0, 1) + '-' + val.slice(1);
                                             }
                                             setSelectedGuruh(val);
                                         }}
